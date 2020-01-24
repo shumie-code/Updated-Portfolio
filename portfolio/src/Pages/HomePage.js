787029b2991d6036c96b901bcd1d7d3c9725b154
Petros Shumie <shumie.peter@gmail.com>
2020-01-23 20:15:37 -0500

    Iported hero and carousel components to be used in the homepage, the sub components will have props that are going to be rendered here.

---
+++ portfolio/src/Pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Hero from '../Components/Hero';
+import Carousel from '../Components/Carousel';
+
+function HomePage(props) {
+
+    return(
+        <div>
+            <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
+            <Carousel />
+        </div>
+    );
+}
+
+export default HomePage;
