7c53a164eb47962523f2bae024041a875a19614e
Petros Shumie <shumie.peter@gmail.com>
2020-01-23 19:55:57 -0500

    Return method contains router utitlty to switch between different links on different pages through the navbar. Navbar contains links that act as a tags in vanilla javascript. Each link leverages React router to different pages.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -38,7 +38,24 @@ class App extends React.Component {
   return () {
     return (
       <Router>
-        <Container className="p-0"
+        <Container className="p-0" fluid={true}>
+
+
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Petros Shumie</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          
+        </Container>
       </Router> 
     )
   }
