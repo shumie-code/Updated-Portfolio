e86b4939dce2d9ca74806411e74e50595cec5d6b
Petros Shumie <shumie.peter@gmail.com>
2020-01-23 20:02:46 -0500

    Link to routes created for Pages and passed state down to component pages to be used later on in code as props.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -54,10 +54,16 @@ class App extends React.Component {
             </Navbar.Collapse>
           </Navbar>
 
-          
-        </Container>
-      </Router> 
-    )
-  }
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/about" render={() => <AboutPage title={this.state.home.title} subTitle={this.state.about.title} />} />
+          <Route path='/contact' render={() => <ContactPage title={this.state.contact.title} />} />
+
+          </Footer />
+          </Container />
+          </Router>
+
+    );
   }
 }
+
+export default App;
