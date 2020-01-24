bd07e5173692260b1afcab3fc93573884f2a46fd
Petros Shumie <shumie.peter@gmail.com>
2020-01-23 19:47:31 -0500

    Created App that extends React.Component, living inside the App component is a  a constructor that creates an object state to pass down props into child components.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,16 +2,45 @@ import React from 'react';
 import {BrowserRouter as Router, Route, link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
+
 import Footer from './Components/Footer';
 import Homepage from './Pages/HomePage'
 import ContactPage from './Pages/ContactPage';
 import AboutPage from './Pages/AboutPage';
 
-function App() {
-  return (
- 
-  );
-}
+class App extends React.Component {
 
-export default App;
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Peter Shumie',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' }
+      ],
+      home: {
+        title: 'Love the Process',
+        subTitle: 'Find problems, Create solutions',
+        text: 'Lets work together'
+      },
+      about: {
+        title: 'Who am I?'
+      },
+      contact: {
+        title: 'Nice to meet you!'
+      }
+    }
+  }
+
+  return () {
+    return (
+      <Router>
+        <Container className="p-0"
+      </Router> 
+    )
+  }
+  }
+}
