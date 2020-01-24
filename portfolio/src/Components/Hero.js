2f5bc1eb52b7041f96f3fa85f91d0c02cf4ff8ac
Petros Shumie <shumie.peter@gmail.com>
2020-01-23 20:35:22 -0500

    Hero component created for text on main page. Imported Row and Col from react-bootsrap to be used in project. Also container and Jumbotron for size of heading and subtitile

--- portfolio/src/Components/Hero.js
+++ portfolio/src/Components/Hero.js
@@ -1,1 +1,6 @@
-import
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
