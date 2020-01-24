a4f97ea31f88e9b71774b878e60fd7bc64553ea8
Petros Shumie <shumie.peter@gmail.com>
2020-01-23 19:34:00 -0500

    Imported component Footer, Home, About and Contact Pages for app.js functionality.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Route, link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
+import Footer from './Components/Footer';
+import Homepage from './Pages/HomePage'
+import ContactPage from './Pages/ContactPage';
+import AboutPage from './Pages/AboutPage';
 
 function App() {
   return (
