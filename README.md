19a9846c50cf1a6ea41dc4b09335c3cde768c3f7
Petros Shumie <shumie.peter@gmail.com>
2020-02-05 11:18:31 -0500

    Reinstalled create-react due to missing dependcies causeing erros.

--- README.md
+++
@@ -1,3 +0,0 @@
-# Updated-Portfolio
-This is an updated portfolio of that I created as a showcase for my work and resume. 
-I have not added links to my page for downloading my resume or linking my git hub assignments yet. 
--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,69 +1,26 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import logo from './logo.svg';
 import './App.css';
 
-import Footer from './Components/Footer';
-import HomePage from './Pages/HomePage';
-import ContactPage from './Pages/ContactPage';
-import AboutPage from './Pages/AboutPage';
-
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: 'Peter Shumie',
-      headerLinks: [
-        { title: 'Home', path: '/' },
-        { title: 'About', path: '/about' },
-        { title: 'Contact', path: '/contact' }
-      ],
-      home: {
-        title: 'Love the Process',
-        subTitle: 'Find problems, Create solutions',
-        text: 'Lets work together'
-      },
-      about: {
-        title: 'Who am I?'
-      },
-      contact: {
-        title: 'Nice to meet you!'
-      }
-    }
-  }
-
-  return () {
-    return (
-      <Router>
-        <Container className="p-0" fluid={true}>
-
-
-          <Navbar className="border-bottom" bg="transparent" expand="lg">
-            <Navbar.Brand>Petros Shumie</Navbar.Brand>
-
-            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
-            <Navbar.Collapse id="navbar-toggle">
-              <Nav className="ml-auto">
-                <Link className="nav-link" to="/">Home</Link>
-                <Link className="nav-link" to="/about">About</Link>
-                <Link className="nav-link" to="/contact">Contact</Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-
-          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
-          <Route path="/about" render={() => <AboutPage title={this.state.home.title} subTitle={this.state.about.title} />} />
-          <Route path='/contact' render={() => <ContactPage title={this.state.contact.title} />} />
-
-          <Footer />
-          </Container>
-          </Router>
-
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
