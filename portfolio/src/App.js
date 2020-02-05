0132fe1765058ca5916b6a6c8422020e4d80d474
Petros Shumie <shumie.peter@gmail.com>
2020-02-05 13:17:19 -0500

    Created HomePage, AboutPage and ContactPage that route via react router. Also passed state down from app.js file to props that will be rendered on the pages themselves as functional components.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route,  Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import NavbarBrand from 'react-bootstrap/NavbarBrand';
 import Nav from 'react-bootstrap/Nav';
 import NavbarToggle from 'react-bootstrap/NavbarToggle';
 import NavbarCollapse from 'react-bootstrap/NavbarCollapse';
+
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage'; 
+
 import './App.css';
 
 class App extends React.Component {
@@ -46,9 +52,14 @@ class App extends React.Component {
             <Link className="nav-link" to="/">Home</Link>
             <Link className="nav-link" to="/about">About</Link>
             <Link className="nav-link" to="/contact">Contact</Link>
+          
           </Nav>
         </NavbarCollapse>
         </Navbar>
+        <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subtitle} text={this.state.home.text} />} />
+        <Route path="/about"  render={() => <AboutPage title={this.state.about.title} />} />
+        <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} /> } />
+        <Footer />
         </Container>
       </Router>
     );
---
+++ portfolio/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function HomePage(props) {
+
+    return (
+        <p>Homepage works!</p>
+    );
+}
+
+export default HomePage;
